a8e05bb8ae08791b1805b0f379c96aa82fa9a598
BadBubble <badbubble>
2022-12-22 13:14:09 +0800

    add token

---
+++ token/token.go
@@ -0,0 +1,31 @@
+package token
+
+type TokenType string
+
+const (
+	/*特殊类*/
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	/*标识符*/
+	INT   = "INT"
+	IDENT = "IDENT"
+	/*运算符*/
+	ASSIGN = "="
+	PLUS   = "+"
+	/*符号*/
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	/*关键字*/
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// Token 通过lexer将代码转换成一个一个的Token
+type Token struct {
+	Type    TokenType // Token的类别
+	Literal string    // Token的字面量
+}
