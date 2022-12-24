6a0ffcb707ac8f6872fa9061667c80237de64522
BadBubble <badbubble>
2022-12-24 19:28:30 +0800

    test more identifiers and double char

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -12,7 +12,17 @@ let ten = 10;
 let add = fn(x, y) {
 	x + y;
 };
-let result = add(five, ten);`
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+10 == 10
+9 != 10
+`
 
 	tests := []struct {
 		ExpectedTokenType token.TokenType
@@ -60,6 +70,49 @@ let result = add(five, ten);`
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		// !-/*5;
+		{token.BAND, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		// 5 < 10 > 5
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		// if (5 < 10) {
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		// return true;
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		// } else {
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		// return false;
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		// }
+		{token.RBRACE, "}"},
+		// 10 == 10
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		// 9 != 10
+		{token.INT, "9"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "10"},
 		// EOF
 		{token.EOF, ""},
 	}
