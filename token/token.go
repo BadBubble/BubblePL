ea15ea1ec5c07a70a42a1ac538a355ac4154abb6
BadBubble <badbubble>
2022-12-24 11:36:52 +0800

    add keywordsmap & LookupIdentifier

--- token/token.go
+++ token/token.go
@@ -24,10 +24,24 @@ const (
 	LET      = "LET"
 )
 
+// KeywordsMap 关键字的Literal到TokenType的映射
+var KeywordsMap = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
 // Token 通过lexer将代码转换成一个一个的Token
 type Token struct {
 	Type    TokenType // Token的类别
 	Literal string    // Token的字面量
+}
+
+// LookupIdentifier 根据字面量查找是否是关键字，如果是关键字就返回关键字的TokenType否则就是IDENT
+func LookupIdentifier(literal string) TokenType {
+	if tokenType, ok := KeywordsMap[literal]; ok {
+		return tokenType
+	}
+	return IDENT
 }
 
 // New 创建Token
