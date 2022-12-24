34989600b995ba38e3376abe2ded8b5ef873db1a
BadBubble <badbubble>
2022-12-24 19:29:04 +0800

    add more identifier

--- token/token.go
+++ token/token.go
@@ -10,8 +10,17 @@ const (
 	INT   = "INT"
 	IDENT = "IDENT"
 	/*运算符*/
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+	BAND     = "!"
+	/*比较符*/
+	LT     = "<"
+	GT     = ">"
+	EQ     = "EQ"
+	NOT_EQ = "NOT_EQ"
 	/*符号*/
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -22,12 +31,22 @@ const (
 	/*关键字*/
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	RETURN   = "RETURN"
 )
 
 // KeywordsMap 关键字的Literal到TokenType的映射
 var KeywordsMap = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
 }
 
 // Token 通过lexer将代码转换成一个一个的Token
