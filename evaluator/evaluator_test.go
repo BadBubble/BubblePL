fedda364bf3cc856664aa33fe3ed6cea83ce4854
badbubble <liuhanjun369@gmail.com>
2023-03-17 17:04:22 +0800

    test evalate integer

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,43 @@
+package evaluator
+
+import (
+	"BubblePL/lexer"
+	"BubblePL/object"
+	"BubblePL/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("obj is not *object.Integer. got=%T", obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object.Integer got wrong value, expected=%d, got=%d.", expected, result.Value)
+		return false
+	}
+	return true
+}
