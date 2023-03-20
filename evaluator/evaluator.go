4406fd412b2d16f2a944e7ffe83e5a69d23cf408
badbubble <liuhanjun369@gmail.com>
2023-03-20 09:33:36 +0800

    evaluate bang operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -25,10 +25,34 @@ func Eval(node ast.Node) object.Object {
 		} else {
 			return FALSE
 		}
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 	return nil
 }
 
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
 func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 	for _, statement := range stmts {
