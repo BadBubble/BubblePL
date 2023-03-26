964d1f871e664bc5d51d726f63d2135862b818d9
badbubble <liuhanjun369@gmail.com>
2023-03-26 13:41:18 +0800

    test  builtin function

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -323,3 +323,35 @@ func TestStringLiteralConcat(t *testing.T) {
 		t.Errorf("got wrong string. got=%s", evaluated)
 	}
 }
+
+func TestBuiltinFunction(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`len("")`, 0},
+		{`len("four")`, 4},
+		{`len("hello world")`, 11},
+		{`len(1)`, "argument to `len` not supported, got=INTEGER"},
+		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Errorf("object is not *Object.Error. got=%T", evaluated)
+				continue
+			}
+
+			if errObj.Message != expected {
+				t.Errorf("wrong error message. expected=%q, got=%q",
+					expected, errObj.Message)
+			}
+		}
+	}
+}
