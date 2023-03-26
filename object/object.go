77fef31696e246c933463a6de134833c7fa145d9
badbubble <liuhanjun369@gmail.com>
2023-03-26 13:41:46 +0800

    add BuiltinObject

--- object/object.go
+++ object/object.go
@@ -17,7 +17,10 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNTION_OBJ      = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
+
+type BuiltinFunction func(args ...Object) Object
 
 type Object interface {
 	Type() ObjectType
@@ -117,4 +120,16 @@ func (s *String) Type() ObjectType {
 
 func (s *String) Inspect() string {
 	return s.Value
+}
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType {
+	return BUILTIN_OBJ
+}
+
+func (b *Builtin) Inspect() string {
+	return "builtin function"
 }
