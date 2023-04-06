d702508db85baa06df70ba5ada9bc05968c4f3b8
badbubble <liuhanjun369@gmail.com>
2023-04-06 11:30:33 +0800

    add HashLiteral to express Hash Node

--- ast/ast.go
+++ ast/ast.go
@@ -370,3 +370,28 @@ func (i *IndexExpression) String() string {
 func (i *IndexExpression) ToLiteral() string {
 	return i.Token.Literal
 }
+
+type HashLiteral struct {
+	Token token.Token
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {
+
+}
+
+func (hl *HashLiteral) ToLiteral() string {
+	return hl.Token.Literal
+}
+
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+	var pairs []string
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	return out.String()
+}
